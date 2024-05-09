64c31d7d6adc3d071b43228071a0f7089171c712
vinay singh kadela <132544545+VijaySinghKadela@users.noreply.github.com>
2024-05-09 21:14:53 +0530

    added

--- script.js
+++ script.js
@@ -9,9 +9,6 @@ document.addEventListener("mousemove",function(dets){
     blur.style.top = dets.y - 200 +"px"
 })
 
-
-
-
 gsap.to("#nav",{
         backgroundColor:"#000",
         height:"110px",
@@ -38,4 +35,37 @@ gsap.to("#main",{
         scrub:2,
     }
 
+})
+
+let h4all = document.querySelectorAll("#nav h4")
+h4all.forEach(function(elem){
+    elem.addEventListener("mouseenter",function(){
+        cursor.style.scale = 3
+        cursor.style.border = "1px solid #fff"
+        cursor.style.backgroundColor = "transparent"
+
+    })
+})
+
+h4all.forEach(function(elem){
+    elem.addEventListener("mouseleave",function(){
+        cursor.style.scale = 1
+        cursor.style.border = "0px solid #95C11E"
+        cursor.style.backgroundColor = "#95C11E"
+
+    })
+})
+
+gsap.from("#about-us img,#about-us-in",{
+    y:90,
+    opacity:0,
+    duration:1,
+    scrollTrigger:{
+        trigger:"#about-us",
+        scroller:"body",
+        markers:true,
+        start:"top 60%",
+        end:"top 55%",
+        scrub:3
+    }
 })
