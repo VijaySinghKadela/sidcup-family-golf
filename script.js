69d9a35a1ab7d6273ab011b604f4d3502734b3c2
vinay singh kadela <132544545+VijaySinghKadela@users.noreply.github.com>
2024-05-09 23:04:18 +0530

    added

--- script.js
+++ script.js
@@ -63,9 +63,62 @@ gsap.from("#about-us img,#about-us-in",{
     scrollTrigger:{
         trigger:"#about-us",
         scroller:"body",
-        markers:true,
-        start:"top 60%",
-        end:"top 55%",
+        // markers:true,
+        start:"top 70%",
+        end:"top 65%",
+        scrub:3
+    }
+})
+
+gsap.from(".card",{
+    scale:0.8,
+    opacity:0,
+    duration:1,
+    stagger:0.1,
+    scrollTrigger:{
+        trigger:".card",
+        scroller:"body",
+        // markers:true,
+        start:"top 70%",
+        end:"top 65%",
+        scrub:3
+    }
+})
+
+gsap.from("#colon1",{
+    x:-70,
+    y:-70,
+    scrollTrigger:{
+        trigger:"#colon1",
+        scroller:"body",
+        // markers:true,
+        start:"top 55%",
+        end:"top 45%",
+        scrub:3
+    }
+})
+
+gsap.from("#colon2",{
+    x:70,
+    y:70,
+    scrollTrigger:{
+        trigger:"#colon2",
+        scroller:"body",
+        // markers:true,
+        start:"top 55%",
+        end:"top 45%",
+        scrub:3
+    }
+})
+
+gsap.from("#page4 h1",{
+    y:50,
+    scrollTrigger:{
+        trigger:"#page4 h1  ",
+        scroller:"body",
+        // markers:true,
+        start:"top 75%",
+        end:"top 70%",
         scrub:3
     }
 })
